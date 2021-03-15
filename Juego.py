9fa68bd3c5c91834d631a0ffd15d7cba0cef9dff
Jairo <jairo.echegoyen@gmail.com>
2021-03-15 09:01:00 +0100

    Código completamente traducido al inglés

--- Juego.py
+++ Juego.py
@@ -4,15 +4,15 @@ import math
 pi = math.pi
 
 #Defining colors
-NEGRO = (0,0,0)
-BLANCO = (255,255,255)
-ROJO = (255,0,0)
-VERDE = (0,255,0)
-AZUL = (0,0,255)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (255,0,0)
+GREEN = (0,255,0)
+BLUE = (0,0,255)
 MAGENTA = (255,0,255)
 CIAN = (0,255,255)
-AMARILLO = (255,255,0)
-NARANJA = (255,153,0)
+YELLOW = (255,255,0)
+ORANGE = (255,153,0)
 
 #Defining classes
 
@@ -37,15 +37,15 @@ class Player(pygame.sprite.Sprite):
 	def updateRight(self):
                 Player.speedX+=1
 		self.rect.x+=Player.speedX
-                reproducir_sonido("step.ogg")
+                playSound("step.ogg")
 	def updateLeft(self):
                 Player.speddX-=1
 		self.rect.x+=Player.speedX
-                reproducir_sonido("step.ogg")
+                playSound("step.ogg")
 	def stop(self):
 		self.rect.x=self.rect.x
 
-class Bala(pygame.sprite.Sprite):
+class Bullet(pygame.sprite.Sprite):
 	def __init__(self, color):
 		super().__init__()
 		self.image = pygame.Surface([3, 1])
@@ -55,17 +55,17 @@ class Bala(pygame.sprite.Sprite):
 		self.rect.x+=6
 
 pygame.init()
-dimensiones = (700,500)
-pantalla = pygame.display.set_mode(dimensiones)
+dimensions = (700,500)
+screen = pygame.display.set_mode(dimensions)
 pygame.mixer.music.load("music.ogg")
 pygame.mixer.music.play(-1, 0.0)
 
 pygame.display.set_caption("Juego")
 
-def reproducir_sonido(sonido):
+def playSound(sound):
 	#Reproduce un sonido, que tiene que estar en la misma carpeta que el archivo
-	golpe = pygame.mixer.Sound(sonido)
-	golpe.play()
+	played = pygame.mixer.Sound(sound)
+	played.play()
 
 enemiesList=pygame.sprite.Group()
 spritesList=pygame.sprite.Group()
@@ -73,35 +73,35 @@ bulletsList=pygame.sprite.Group()
 enemiesHitList=[]
 
 for x in range(50):
-	enemy=Enemy(VERDE)
+	enemy=Enemy(GREEN)
 	enemy.rect.x=random.randint(350, 700)
 	enemy.rect.y=490
 	enemiesList.add(enemy)
 	spritesList.add(enemy)
 
-player=Player(AMARILLO)
+player=Player(YELLOW)
 player.rect.x=0
 player.rect.y=490
 spritesList.add(player)
 
-fin=False
+end=False
 timer=pygame.time.Clock()
-while not fin:
+while not end:
 	for event in pygame.event.get():
 		if event.type == pygame.quit:
-			fin=true
+			end=true
 		elif event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_LEFT:
 				player.updateLeft()
 			if event.type == pygame.K_RIGHT:
 				player.updateRight()
 			if event.type == pygame.K_SPACE:
-				bullet=Bala(RED)
+				bullet=Bullet(RED)
 				bullet.rect.x=player.rect.x
 				bullet.rect.y=player.rect.y
 				spritesList.add(bullet)
 				bulletsList.add(bullet)
-				reproducir_sonido("shoot.ogg")
+				playSound("shoot.ogg")
 		elif event.type == pygame.KEYUP:
 			if event.key == pygame.K_LEFT:
 				player.stop()
@@ -118,8 +118,8 @@ while not fin:
 			spritesList.remove(enemy)
 
 
-	pantalla.fill(BLANCO)
-	spritesList.draw(pantalla)
+	screen.fill(WHITE)
+	spritesList.draw(screen)
 	pygame.display.flip()
 	timer.tick(60)
 
